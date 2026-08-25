9520281db61451e6825a38221138ee9557003fee
ruheyun <1654033661@qq.com>
2026-08-25 19:16:49 +0800

    20260825001

---
+++ farg.scm
@@ -0,0 +1,7 @@
+(define hello 
+    (lambda (name)
+        (string-append "hello" name "!")))
+
+(define sum3
+    (lambda (a b c)
+        (+ a b c)))
---
+++ test03.scm
@@ -0,0 +1,8 @@
+(define add1
+	(lambda (a)
+		(+ a 1)))
+
+(define minus1
+	(lambda (a)
+		(- a 1)))
+
