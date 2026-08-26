5bcb737f958c215d1a2e36062861746473034994
ruheyun <1654033661@qq.com>
2026-08-26 10:59:23 +0800

    20260826

--- test04.scm
+++ test04.scm
@@ -3,7 +3,7 @@
 		(* a0
 			(if (= r 1)
 				n
-				(/ (-1 (expt r n)) (- 1 r))))))
+				(/ (- 1 (expt r n)) (- 1 r))))))
 
 (define abs1
 	(lambda (a)
@@ -21,7 +21,7 @@
 	(lambda (a)
 		(if (> a 33)
 			(if (< a 126)
-				(interger->char a)
+				(integer->char a)
 				#f)
 			#f)))
 
@@ -46,5 +46,22 @@
 		(if (or (fs a) (fs b) (fs c))
 			(* a b c))))
 
+(define foo
+	(lambda (age)
+		(cond
+			((or (<= age 3) (>= age 65)) 0)
+			((<= 4 age 6) 0.5)
+			((<= 7 age 12) 1.0)
+			((<= 13 age 15) 1.5)
+			((<= 16 age 18) 1.8)
+			(else 2.0))))
+
+(define cj
+	(lambda (score)
+		(cond
+			((>= score 80) "A")
+			((<= 60 score 79) "B")
+			((<= 40 score 59) "C")
+			(else "D"))))
 
 
